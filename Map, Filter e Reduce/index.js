f76b716b3a7162083aeb1776c1a2b512fce23de1
Eugênia Rodrigues <eugenyarodrigues@gmail.com>
2022-05-16 11:15:37 -0300

    arquivos2

--- Map, Filter e Reduce/index.js
+++ Map, Filter e Reduce/index.js
@@ -1,3 +1,5 @@
+/*
+
 const maca = {
     value:  2,
 }
@@ -17,3 +19,104 @@ const nums = [1, 2];
 
 console.log('this -> maçã', mapComThis(nums, maca));
 console.log('this -> laranja', mapComThis(nums, laranja));
+
+*/
+
+/*
+
+function mapSemThis(arr) {
+    return arr.map(function(item){
+        return item * 2;
+    }); 
+    }
+    
+    const nums = [2, 4, 6, 8, 10];
+    
+    console.log(mapSemThis(nums));
+
+*/
+
+/*
+
+function filtrarPares(arr) {
+    return arr.filter(callback)
+}
+
+function callback(item) {
+    return item % 2 === 0;
+
+}
+
+const meuArray = [1,23, 55, 67, 30, 2, 4];
+
+console.log(filtrarPares(meuArray));
+
+*/
+
+/*function filtrarPares(arr) {
+    return arr.filter(function callback(item){
+        return item % 2 === 0;
+    })
+}
+
+const meuArray = [1,23, 55, 67, 30, 2, 4];
+
+console.log(filtrarPares(meuArray));
+
+outra forma de fazer*/
+
+/*
+
+function filtrarImpares(arr) {
+    return arr.filter(function callback(item){
+        return item % 2 !== 0;
+    })
+}
+
+const meuArray = [1,23, 55, 67, 30, 2, 4];
+
+console.log(filtrarImpares(meuArray));
+
+outra forma de fazer*/
+
+/*
+
+function somaNumeros(arr) {
+  return  arr.reduce(function(prev, current) {
+        console.log({prev});
+        console.log({current});
+        return prev + current;
+    }, 0);
+}
+
+const arr = [1, 2];
+
+console.log(somaNumeros(arr));
+
+*/
+
+
+const lista = [
+    {
+     name: 'sabao em po',
+     preco: 30,   
+    },
+    {
+        name: 'cereal',
+        preco: 12,
+    },
+    {
+        name: 'toalha',
+        preco: 30,
+    },
+];
+
+const saldoDisponivel = 100;
+
+function calculaSaldo(saldoDisponivel, lista) {
+    return lista.reduce(function (prev, current) {
+        return prev - current.preco;
+    }, saldoDisponivel);
+}
+
+console.log(calculaSaldo(saldoDisponivel, lista));
